031f8e28fc3f051608837599c89a5a2322414ac0
Serge Rey <sjsrey@gmail.com>
2006-07-22 23:27:13 +0000

    got rid of credit card option.

--- donate.php
+++ donate.php
@@ -15,8 +15,7 @@ repositories, the user forums, documnentation and related activities.
 </p>
 <br>
 <p>
- You can donate via paypal or with a major credit card by simply clicking on one 
-of the following buttons. 
+ You can donate via paypal by simply clicking on the following button. 
 </p>
 <br>
 <table>
@@ -32,20 +31,6 @@ of the following buttons.
 <input type="hidden" name="tax" value="0">
 <input type="hidden" name="bn" value="PP-DonationsBF">
 <input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
-<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
-</td>
-<td>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_xclick">
-<input type="hidden" name="business" value="sjsrey@gmail.com">
-<input type="hidden" name="item_name" value="gtd-php">
-<input type="hidden" name="no_shipping" value="2">
-<input type="hidden" name="no_note" value="1">
-<input type="hidden" name="currency_code" value="USD">
-<input type="hidden" name="tax" value="0">
-<input type="hidden" name="bn" value="PP-DonationsBF">
-<input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-butcc-donate.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
 <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 </td>
