fd19bf4b4891f2eaa419a6df590ccadeef0965de
Serge Rey <sjsrey@gmail.com>
2006-07-31 19:58:55 +0000

    added josh to the development team.

--- credits.php
+++ credits.php
@@ -12,6 +12,7 @@ Development Team
 	</TR>
 	<tr><td><a href="mailto:sjrey@users.sf.net?subject=gtd-php">Serge Rey</a></td><td>Project Founder and Director</td></tr>
 	<tr><td><a href="mailto:skorvek@gtd-php.com?subject=gtd-php">Scott Jason Korvek</a></td><td>GTD Enhancements</td></tr>
+	 <tr><td><a href="http://burnettsonline.org/">Josh Burnett</a></td><td>CSS</td></tr> 	  
 </table>
 
 Contributors
@@ -21,7 +22,6 @@ Contributors
 		<TH>What</TH>
 	</TR>
 	<tr><td><a href="http://jason.br.com/">Jason Brown</a></td><td>CSS and design</td></tr>
-	 <tr><td><a href="http://burnettsonline.org/">Josh Burnett</a></td><td>CSS</td></tr> 	  
 	<tr><td><a href="mailto:halonine@users.sourceforge.net?subject=gtd-php">Mike Kober</a></td><td>CSS (IE Savior)</td></tr>
 </table>
 
